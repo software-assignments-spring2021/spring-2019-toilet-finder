c2cba096deb0ae95eb11be1050b93c75f63c1371
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-03-05 11:38:49 -0500

    added to test/test.js

--- src/test/test.js
+++ src/test/test.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import App from '../App.js';
+import HomeScreen from '../HomeScreen.js';
+import AddLocationScreen from '../AddLocationScreen.js';
+import renderer from 'react-test-renderer';
 //Yuankai's Tests
 
 
@@ -5,6 +10,15 @@
 
 
 //Nicholas's Tests
+// Screen rendering test
+it('HomeScreen renders correctly', () => {
+	const tree = renderer.create(<View />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
 
+it('AddLocationScreen renders correctly', () => {
+	const tree = renderer.create(<View />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
 
 //Justin's Tests
