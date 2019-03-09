6ba820ec1ca9f4514a42625b8f2353635e1de011
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-03-09 17:16:20 -0500

    added Map.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import AddLocationScreen from './screens/AddLocationScreen';
 const BottomTabNavigator = createBottomTabNavigator({
   Home: { screen: HomeScreen },
   Add: { screen: AddLocationScreen },
+  Map: { screen: Map },
 });
 
 const App = createAppContainer(BottomTabNavigator);
 
 export default App;
-
