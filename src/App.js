5881d9aaefa047b158b4231f413df564528216e9
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-03-10 14:52:57 -0400

    integrated maps and added find button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import MapView from 'react-native-maps';
 
-import HomeScreen from './screens/HomeScreen';
-import AddLocationScreen from './screens/AddLocationScreen';
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex:1}}>
+        <MapView
+          style={styles.map}
+          provider="google"
 
-const BottomTabNavigator = createBottomTabNavigator({
-  Home: { screen: HomeScreen },
-  Add: { screen: AddLocationScreen },
-  Map: { screen: Map },
+          initialRegion={{
+            latitude: 40.76727216,
+            longitude: -73.99392888,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        ></MapView>
+        <Button
+          onPress={this.handlePress}
+          style={styles.findButton}
+          title="Find The Nearest Bathroom"
+          color="red"
+        />
+      </View>
+    );
+  }
+
+  handlePress = () => {
+    // Process current location and search for bathrooms
+  }
+
+  
+}
+
+const styles = StyleSheet.create({
+  findButton: {
+    // Add styles to the search button
+  },
+  map: {
+    zIndex: -1,
+    flex: 1
+  },
 });
-
-const App = createAppContainer(BottomTabNavigator);
-
-export default App;
