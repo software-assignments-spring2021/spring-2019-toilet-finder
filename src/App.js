c69e2be8203215fc082d4810e022dd1d95e27b3a
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-03-11 16:06:40 -0400

    added geolocation to button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Platform,
+  TouchableOpacity } from "react-native";
 import MapView from 'react-native-maps';
+import { Constants, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Error with Android Emulator, try on your device',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
   render() {
+
+    let text = "Loading";
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+    console.log(text);
     return (
       <View style={{flex:1}}>
         <MapView
           style={styles.map}
           provider="google"
-
           initialRegion={{
             latitude: 40.76727216,
             longitude: -73.99392888,
@@ -18,7 +59,10 @@ export default class App extends React.Component {
           }}
         ></MapView>
         <Button
-          onPress={this.handlePress}
+          onPress={() => {
+            if (Location.hasServicesEnabledAsync())
+              Alert.alert(text);
+          }}
           style={styles.findButton}
           title="Find The Nearest Bathroom"
           color="red"
@@ -28,10 +72,8 @@ export default class App extends React.Component {
   }
 
   handlePress = () => {
-    // Process current location and search for bathrooms
+    Alert.alert("you tapped the button!");
   }
-
-  
 }
 
 const styles = StyleSheet.create({
