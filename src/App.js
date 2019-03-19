4ca80feb3aaf10b84a69f37ffc31cbf0096c05b7
Yuankai Shan <shan.yuankai@gmail.com>
2019-03-18 23:38:20 -0400

    can now write to the database, data written still needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-var AWS = require('aws-sdk')
-
 import { createBottomTabNavigator, createAppContainer} from 'react-navigation';
 
 import HomeScreen from './screens/HomeScreen';
@@ -14,16 +12,6 @@ const App = createAppContainer(BottomTabNavigator);
 
 export default App;
 
-// Initialize the Amazon Cognito credentials provider
-AWS.config.region = 'us-east-1'; // Region
-var creds = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:e7994f82-231f-43db-9a9b-e1868280592f',
-});
-
-AWS.config.credentials = creds;
-
-var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
 // var params = {
 //   TableName: "test",
 //   Item: {
@@ -31,19 +19,7 @@ var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 //   }
 // }
 
-// dynamodb.putItem(params, (err, data) => {
-//   if (err) {
-//     console.error("Unable to add item. ");
-//   } else {
-//     console.log("Adding item:");
-//   }
-// })
 
-// ddb.listTables({Limit: 10}, function(err, data) {
-//   if (err) {
-//     console.log("Error", err.code);
-//   } else {
-//     console.log("Table names are ", data.TableNames);
-//   }
-// });
+
+
 
