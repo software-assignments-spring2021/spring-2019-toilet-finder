0d95dfb16d1163dabae8952217fc1f2c255cf82b
Esther Sun <25215127+esthersun98@users.noreply.github.com>
2019-04-14 04:05:29 -0400

    Update test.js

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from '../App.js';
 import HomeScreen from '../screens/HomeScreen.js';
-import AddLocationScreen from '../screens/AddLocationScreen.js';
+import AddLocationScreen from '../screens/AddLocationScreen';
 import renderer from 'react-test-renderer';
 
 //Yuankai's Tests
