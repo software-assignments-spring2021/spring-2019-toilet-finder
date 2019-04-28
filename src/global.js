40d84214c0a83d416ff05e74678b2a7873e2ad96
Yuankai Shan <shan.yuankai@gmail.com>
2019-04-28 18:34:32 -0400

    all current necessary queries for data written as functions in global.js

--- src/global.js
+++ src/global.js
@@ -19,6 +19,9 @@ AWS.config.credentials = global.creds;
 // database connection
 var ddb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
+
+// For all of the following get functions, pass in the longLat string  
+
 function getLocationData(latLong) {
 
   // query parameters
@@ -48,22 +51,18 @@ function getLocationData(latLong) {
   });
 }
 
-function getTags(latLong) {
+function getTagData(latLong) {
 
   // query parameters
   var params = {
     TableName: "toilets",     
-    ExpressionAttributeVariable: {                  // set string for use in expressions
-      ":latLong": {
-        S: latLong
-      },
-      ":spec": {
-        S: "tag"
-      }
+    ExpressionAttributeValues: {                  // set string for use in expressions
+      ":latLong": latLong,
+      ":spec": "tag"
     },
-    KeyConditionExpression: "long_lat = :latLong",  // partition key comparison
+    KeyConditionExpression: "longLat = :latLong",  // partition key comparison
     FilterExpression: "spec_type = :spec",          // filter my loc to get all locations
-    ProjectionExpression: ""
+    ProjectionExpression: "baby, disabled, paytouse, unisex"
   };
 
   // query database 
@@ -72,12 +71,73 @@ function getTags(latLong) {
       console.log(err);
       return [];          // return empty array if no data so nothing breaks...
     } else {  
+      console.log(data)
       return data.Items;  // return array of location items if query works
     }
   });
 }
 
-export { getLocationData }
+function getRatingData(latLong) {
+
+  // query parameters
+  var params = {
+    TableName: "toilets",     
+    ExpressionAttributeValues: {                  // set string for use in expressions
+      ":latLong": latLong,
+      ":spec": "rating"
+    },
+    KeyConditionExpression: "longLat = :latLong",  // partition key comparison
+    FilterExpression: "spec_type = :spec",          // filter my loc to get all locations
+    ProjectionExpression: "upvote, downvote"
+  };
+
+  // query database 
+  ddb.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return [];          // return empty array if no data so nothing breaks...
+    } else {  
+      console.log(data)
+      return data.Items;  // return array of location items if query works
+    }
+  });
+}
+
+function getDescription(latLong) {
+
+  // query parameters
+  var params = {
+    TableName: "toilets",   
+    ExpressionAttributeNames: {
+      "#desc": "desc"
+    },  
+    ExpressionAttributeValues: {                  // set string for use in expressions
+      ":latLong": latLong,
+      ":spec": "desc"
+    },
+    KeyConditionExpression: "longLat = :latLong",  // partition key comparison
+    FilterExpression: "spec_type = :spec",          // filter my loc to get all locations
+    ProjectionExpression: "#desc"
+  };
+
+  // query database 
+  ddb.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return [];          // return empty array if no data so nothing breaks...
+    } else {  
+      console.log(data)
+      return data.Items;  // return array of location items if query works
+    }
+  });
+}
+
+export { 
+  getLocationData, 
+  getTagData,
+  getRatingData,
+  getDescription
+}
 
 
 
