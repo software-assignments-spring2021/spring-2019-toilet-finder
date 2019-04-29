fe44260188938803d8e14c38acde9165aa9d8eda
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-04-29 09:48:10 -0400

    added rating buttons

--- src/screens/MarkerInfo.js
+++ src/screens/MarkerInfo.js
@@ -19,12 +19,13 @@ export default class MarkerInfo extends React.Component {
 			<Container>
 			<Text style={{fontWeight: 'bold', fontSize: 30, paddingBottom: 15}}>Bathroom: {this.params.name}</Text>
 				<Content style={{alignItems: 'center'}}>
-					<Text style={{fontSize: 20, marginLeft: 35}}>Rating %</Text>
+					<Text style={{fontSize: 20, marginLeft: 35}}>Rating %{this.params.rating}</Text>
 					<View style={{flexDirection: "row"}}>
 					<Button success style={{marginRight: 10}}><Text> Like </Text></Button>
 					<Button danger><Text> Dislike </Text></Button>
 					</View>
-					<Text>Disabled: {this.params.disabled}</Text>
+					<Text>tags: {this.params.tags}</Text>
+					<Text>description: {this.params.description}</Text>
 				</Content>
 
 
